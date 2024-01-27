dad6535cc5a29a3ce31b0f376c44d9dc55146197
Cataldo Luciano <lucianocataldo04@hotmail.com>
2024-01-27 09:10:22 -0500

    conditions fini,debut questionnaire fonction

--- test2.py
+++
@@ -1,9 +0,0 @@
-#13. Calculate the volume of a sphere using the formula (4π/3) x R³. The radius is 10.
-# Reponse a l'exercices
-
-pi = 3.1415926535897931
-r = 10
-calcul_volume = 4.0/3.0 * pi * r**3
-# le calcul effectuer est 4:3 x pi exposé en 3
-
-print("le resultat est: " , calcul_volume)
