f16c04070d09a210f357c84a603b978b981f610c
Cataldo Luciano <lucianocataldo04@hotmail.com>
2024-01-27 09:09:19 -0500

    conditions fini,debut questionnaire fonction

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-x = 10
-resultat_type = type(x)
-print(resultat_type)
+devise = (input("Votre devise est E ou $ ?"))
+
+if devise == "E": 
+    montant_e = float(input("Quel est votre montant en euros ? :"))
+    taux_dollars = 1.09
+    montant_dollars = montant_e * taux_dollars 
+    print(f"{montant_e} euros équivalent a {montant_dollars} dollars")
+elif devise == "$": 
+     montant_dollars = float(input("Quel est votre montant en dollars ? :"))
+     taux_euro = 0.91
+     montant_e = montant_dollars / taux_euro
+     print(f"{montant_dollars} dollars équivalent a {montant_e} euros")
+     
+else: 
+     print("choix de devises non valide")
+
